5d1a664c08ae549aa7b906b030ceb13cd5615e44
Valeria Salas <v.salasguz@gmail.com>
2021-09-09 21:06:32 -0300

    speed smooth scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-var scroll = new SmoothScroll('a[href*="Primera"]')
-var scroll = new SmoothScroll('a[href*="Segunda"]')
-var scroll = new SmoothScroll('a[href*="Tercera"]')
+var scroll = new SmoothScroll('a[href*="Primera"]', {
+    speed:200,
+});
+var scroll = new SmoothScroll('a[href*="Segunda"]', {
+    speed:200,
+});
+var scroll = new SmoothScroll('a[href*="Tercera"]', {
+    speed:200,
+})
